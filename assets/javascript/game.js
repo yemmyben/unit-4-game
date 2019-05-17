92de6ee690edc28c0a8215aace49e6f0e2c65fcc
yemmyben <yemmyben@yahoo.co.uk>
2019-05-16 21:41:50 -0400

    working code - need some styling in my css file

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+var random_result;
+var lost = 0;
+var win = 0;
+var previous = 0;
+
+function resetAndStart() {
+
+$('.crystals').empty();
+
+$('#previous').empty();
+
+random_result = Math.floor(Math.random() * 101) + 19;
+
+$('#result').html('Random Result: ' + random_result);
+
+for (var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 11) + 1;
+console.log(random);
+    var crystal = $('<div>');
+        crystal.attr({
+            'class': 'crystal',
+            'data-random': random
+        });
+
+    $(".crystals").append(crystal);
+
+    }
+}
+
+resetAndStart();
+
+$(document).on('click', '.crystal', function() {
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+    $('#previous').html(previous);
+    
+    if (previous > random_result) {
+
+        lost++;
+
+        $('#loss').html('Losses: ' + lost);
+
+        previous = 0;
+
+        resetAndStart();
+
+    } else if (previous === random_result) {
+        win++;
+
+        $('#win').html('Wins: ' + win);
+
+        previous = 0;
+
+        resetAndStart();
+    }
+});
